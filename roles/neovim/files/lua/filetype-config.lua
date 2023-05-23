91d8eff20f97e2740d2cb94106291c145beb7c14
jroddev <jroddev@gmail.com>
2023-05-23 21:11:51 +1000

    fix: unknown function dist#ft#SetFileTypeSH

--- roles/neovim/files/lua/filetype-config.lua
+++ roles/neovim/files/lua/filetype-config.lua
@@ -1,7 +1,16 @@
+
+-- https://github.com/nathom/filetype.nvim/issues/68#issuecomment-1029837698
+local function bash_literal()
+    vim.bo.filetype = 'sh'
+    vim.b.is_bash = true
+end
+
 require("filetype").setup({
     overrides = {
         complex = {
             [".*j2"] = "yaml",
+            [".*.sh"] = bash_literal,
+            [".zsh*"] = bash_literal,
         }
     }
 
